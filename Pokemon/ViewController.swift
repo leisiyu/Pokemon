849504ed010be1375cea250d987fc2f8e198044e
Qianwen Lyu <olivial@Qianwens-iMac.local>
2022-06-24 09:38:38 +0100

    create project

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Qianwen Lyu on 24/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
